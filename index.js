cdeef226969ada16b6f9630d90517f6613987a99
Pablo Garcia <pgarciacamou@gmail.com>
2014-11-22 21:27:35 -0800

    Removed hash bang and allowed app to run on express

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express'),
 app = express();
-app.set('view engine', 'html');
+// app.set('view engine', 'html');
+
+app.use(express.static(__dirname));
 app.get('/', function(req, res) {
-    res.sendfile('index.html', {root: __dirname })
+	res.sendfile('index.html', {root: __dirname })
 });
 var server = app.listen(process.env.PORT || 80);
